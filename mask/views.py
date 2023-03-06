b9c68e7ccd86144fd7b42a455ae3db0ecea69458
loki7778 <lokeshreddy7078@gmail.com>
2023-03-07 00:01:40 +0530

    first commit

---
+++ mask/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def ram(request):
+    return HttpResponce('<h1>my name is ram sir!</h1>')
+
+
